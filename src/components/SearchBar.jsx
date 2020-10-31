b25cd53ad874018520ffc401953412d9b96d1c82
jaimebaik <jaimebaik5@gmail.com>
2020-10-31 14:24:05 -0700

    added components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React from 'react';
 // import { render } from 'react-dom'
 
 //render 
 
 const SearchBar = props => {
-
     //div for the search input 
     return (
         <div id="searchInput">
-            <form>
-                <button> test</button>
+            <form method='POST' action='/product'>
+                <input type='text' placeholder='Search your product here'></input>
+                <button>Search</button>
             </form>
         </div>
     )
 
 }
+//onClick={
+    //fetch(url)
+    //.then(data.json -> components)
+    //backend -> frontend
+
+    //fetch data from json file
+    //return data(image source url...) - shaped in server
+    //stored in state
+    //pass down the state into product
+    //set attribute to render
+
+    
+//}
 
 export default SearchBar
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const UserInput = props => {
+    return (
+        <div className="userInput">
+          <h1>User Input</h1>
+        </div>
+    )
+}
+
+
+export default UserInput
