bbaf3341b496e2a19e8347c3329c23c193491fe3
jaimebaik <jaimebaik5@gmail.com>
2020-10-31 16:36:11 -0700

    connected backend and frontend

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -4,17 +4,19 @@ const path = require('path');
 const mainController = {};
 
 mainController.getProducts = (req, res, next) => {
-  const { results } = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json'), 'UTF-8'));
-
-  if (!results) {
+  // const {name} = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json'), 'UTF-8'));
+  const result = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json'), 'UTF-8'));
+  console.log("I'm in main controller");
+  console.log(result);
+  if (!result) {
     return next({
-      log: 'fileController.getCharacters: ERROR: Error getting characters data from characters.json file',
-      message: { err: 'Error occurred in fileController.getCharacters. Check server logs for more details.' },
+      log: 'mainController.getProducts: ERROR: Error getting products data from products.json file',
+      message: { err: 'Error occurred in mainController.getProducts. Check server logs for more details.' },
     });
   }
 
-  res.locals.products = results;
-  next();
+  res.locals.products = result;
+  return next();
 }
 
 
@@ -22,5 +24,6 @@ mainController.getProducts = (req, res, next) => {
 // use params for ingredients
 // use params for products
 
+module.exports = mainController;
 
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,8 +3,11 @@ const mainController = require('../controllers/mainController');
 const router = express.Router();
 
 // ADD STARTER DATA REQUEST ROUTE HANDLER HERE
-router.get('/', mainController.getProducts, (req, res) => {
-  return res.status(200).json({products: res.locals.products,});
+router.get('/',mainController.getProducts,(req, res) => {
+  console.log("I'm in api router")
+  //  
+  // return res.status(200).json(res.locals.products);
+  return res.status(200);
 })
 
 // router.post('/', )
